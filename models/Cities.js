a050e9f84918c7d7088e9e8edf294bb7f588fa70
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-08-31 18:43:33 -0300

    B5

--- models/Cities.js
+++ models/Cities.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const citiesSchema = new mongoose.Schema({
     city: {String, required: true},
-    photo: {String, required: true},
+    image: {String, required: true},
     country:{String, required: true},
     description:{String, require: true},
     population:{Number, min: 1000, max: 100000000,require: true},
@@ -11,7 +11,6 @@ const citiesSchema = new mongoose.Schema({
 
 })
 
-const Cities = mongoose.model('cities', citiesSchema)
-module.exports = Cities
+module.exports = mongoose.model('cities', citiesSchema)
     //nombre de la colección
     //esquema de datos
