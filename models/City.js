dbd9c6c9b7c04ee32b9481898fe4b068ba3bf928
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-01 03:28:33 -0300

    Update City.js

--- models/City.js
+++ models/City.js
@@ -6,7 +6,7 @@ const citiesSchema = new mongoose.Schema({
     country:{type: String, required: true},
     description:{type: String, require: true},
     population:{type: Number, min: 1000, max: 100000000,require: true},
-    fundation:{type: Date, min: -9999, max: 9999, require: true},
+    fundation:{type: Date, min: 1000, max: 2022, require: true},
 })
 
 module.exports = mongoose.model('City', citiesSchema)
