e91c70a89a7d03c59b768b14f85e0f5f1bee1631
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-07 03:08:45 -0300

    new Itinerary fix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const citiesRouter = require('./cities')
 const usersRouter = require('./users')
+const itinerariesRouter  = require('./itinerary')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'My Tinerary'});
@@ -9,5 +10,6 @@ router.get('/', function(req, res, next) {
 
 router.use('/cities',citiesRouter)
 router.use('/user',usersRouter)
+router.use('/itineraries',itinerariesRouter)
 
 module.exports = router;
