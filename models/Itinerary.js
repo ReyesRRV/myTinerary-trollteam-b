7aae714eeb4dd60ed359a53a8c71207db67b91e9
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-08 09:33:56 -0300

    itinerary fixeds

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 
 const itinerarySchema = new mongoose.Schema({
     name: {type: String, required: true},
-    user: {type: String, required: true},
-    city:{type: String, required: true},
-    price:{type: Number, require: true},
-    likes:{type: Array, require: true},
-    tags:{type: Array, require: true},
-    duration:{type: String, require: true},
+    user: {type: mongoose.Types.ObjectId, ref:"users", required: true},
+    city:{type: mongoose.Types.ObjectId, ref:"City" , required: true},
+    price:{type: Number, min: 1 , max: 5 , require: true},
+    likes:{type: Array, require: false},
+    tags:{type: Array, require: false},
+    duration:{type: Number, require: true},
 })
 
 module.exports = mongoose.model('Itinerary', itinerarySchema)
