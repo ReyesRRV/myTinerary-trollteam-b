f27c574c86b7c38f0b1e508649480f3face699ff
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-10 23:58:39 -0300

    fix

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -5,8 +5,8 @@ const itinerarySchema = new mongoose.Schema({
     user: {type: mongoose.Types.ObjectId, ref:"users", required: true},
     city:{type: mongoose.Types.ObjectId, ref:"City" , required: true},
     price:{type: Number, min: 1 , max: 5 , require: true},
-    likes:{type: Array, require: false},
-    tags:{type: Array, require: false},
+    likes:{type: Array, require: true},
+    tags:{type: Array, require: true},
     duration:{type: Number, require: true},
 })
 
